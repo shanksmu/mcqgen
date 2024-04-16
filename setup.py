592492f3626f5e19c965bfbb6801e63e2f4dcad6
shanksmu <shanksmu@gmail.com>
2024-04-16 16:00:40 -0400

    added modular coding

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='0.0.1',
     author='Shashank Bhardwaj',
     author_email='shanksmu@gmail.com',
-    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2","langchain-community"],
     packages=find_packages()
 )
---
+++ streamlitapp.py
@@ -0,0 +1,10 @@
+import os
+import json
+import traceback
+import pandas as pd
+from dotenv import load_dotenv
+from src.mcqgenerator.utils import read_file, get_table_data
+import streamlit as st
+from langchain.callbacks import get_openai_callback
+from src.mcqgenerator.MCQGenerator import generate_evaluate_chain
+from src.mcqgenerator.logger import logging
